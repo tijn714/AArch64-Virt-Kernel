cb8d60d58232a3389421aca08afa189919e71c3c
Tijn Rodrigo <71492011+tijn714@users.noreply.github.com>
2025-06-16 16:21:46 +0200

    Delete drivers directory

--- drivers/rtc.c
+++
@@ -1,8 +0,0 @@
-#include "common/common.h"
-
-#define PL031_RTC_BASE 0x09010000
-#define RTC_DR (*(volatile uint32_t *)(PL031_RTC_BASE + 0x00)) // Data Register
-
-uint32_t rtc_get_epoch(void) {
-    return RTC_DR;
-}
