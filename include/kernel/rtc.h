014cf09b3f77c461c1b31a4cbe21c45e397293e4
Tijn Rodrigo <71492011+tijn714@users.noreply.github.com>
2025-06-16 16:22:00 +0200

    Delete include directory

--- include/kernel/rtc.h
+++
@@ -1,8 +0,0 @@
-#ifndef RTC_H
-#define RTC_H
-
-#include "common/common.h"
-
-uint32_t rtc_get_epoch(void);
-
-#endif
--- include/kernel/uart.h
+++
@@ -1,9 +0,0 @@
-#ifndef UART_H
-#define UART_H
-
-#include "common/common.h"
-
-void uart_putc(const char c);
-int uart_printk(const char *fmt, ...);
-
-#endif // UART_H
